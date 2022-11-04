713ca79237b94f226a9f8ec8815a22f1e6144e76
robertvoss <vossrobertt@gmail.com>
2022-11-04 19:21:55 -0300

    conclusao do estudo
    menu criado com as routes definidas

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -4,13 +4,15 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "../../views/examples/Home"
 import About from "../../views/examples/About"
+import Param from "../../views/examples/Param";
+import NotFound from "../../views/examples/NotFound";
 
 const Content = props => (
     <main className="Content">
         <Routes >
             <Route path="/about" element={<About />} />
-            <Route path="/" exact element={<Home />} />
             <Route path="/param/:id" element={<Param />} />
+            <Route path="/" exact element={<Home />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
     </main>
